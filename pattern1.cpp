abc4d64792a62ebe8af55b7c91a7e3881d567f02
Vishal Jha <mailkaro0121@gmail.com>
2023-05-13 22:57:11 +0530

    star pattern pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -5,17 +5,15 @@ int main(){
 
 	int n = 4;
 
-	char ch = 'A';
-
 	for(int i = 0; i<n; i++){
-		ch = 'A'+n-i-1;	
-		for(int j = 0; j<=i; j++){
-			cout << ch << " ";
-			ch++;
+		for(int j = 0; j<n; j++){
+			if(j>=n-i-1){
+				cout << "*";
+			}else{
+				cout << " ";
+			}
 		}
-
 		cout << endl;
-		
 	}
 	
 	return 0;
