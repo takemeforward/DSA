fe4dee33253f22e3ea87946ef26b1f27646c4848
Vishal Jha <mailkaro0121@gmail.com>
2023-05-13 22:45:24 +0530

    A BC CDE DEFG pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main(){
 
-	int n = 3;
+	int n = 5;
 
 	char ch = 'A';
 
-	for(int i = 0; i<n; i++){	
+	for(int i = 0; i<n; i++){
+		ch = 'A'+i;	
 		for(int j = 0; j<=i; j++){
 			cout << ch << " ";
-
+			ch++;
 		}
-		ch++;
+
 		cout << endl;
 		
 	}
