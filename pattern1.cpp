cd3ab3f7c90f67b6e8c7c0d63608503aac72df21
Vishal Jha <mailkaro0121@gmail.com>
2023-05-13 22:48:40 +0530

    D CD BCD ABCD pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
 
-	int n = 5;
+	int n = 4;
 
 	char ch = 'A';
 
 	for(int i = 0; i<n; i++){
-		ch = 'A'+i;	
+		ch = 'A'+n-i-1;	
 		for(int j = 0; j<=i; j++){
 			cout << ch << " ";
 			ch++;
