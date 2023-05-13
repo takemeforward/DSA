cb2daaf5de3830d77a265c02bd01efb286fd1803
Vishal Jha <mailkaro0121@gmail.com>
2023-05-13 22:29:49 +0530

    1 21 321 pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -5,12 +5,14 @@ int main(){
 
 	int n = 5;
 
-	int count = 1;
+	int count;
 
 	for(int i = 1; i<=n; i++){
+		count = i;
 		for(int j = 0; j<i; j++){
+			
 			cout << count << " ";
-			count++;
+			count--;
 		}
 		cout << endl;
 	}
